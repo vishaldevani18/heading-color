dc43fe4184647c773e9252c6e34c77481989d469
theonetech137 <theonetech137@theonetech.local>
2020-09-01 13:51:45 +0530

    Add Color Blue

--- example/src/App.js
+++ example/src/App.js
@@ -5,25 +5,15 @@ import  {ColoredHeadingOne ,ColoredHeadingTwo }  from 'heading-color';
 class App extends React.Component{
 
   render(){
-
     return(
-
       <div>
         <ColoredHeadingOne
         text = "Color Heading is working"
-        color={"red"}
-        />
-
-        {/* othor component */}
-        <ColoredHeadingOne
-        text = "Color Heading 2  is working"
         color={"blue"}
         />
-
       </div>
     )
   }
-
 
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -2,28 +2,7 @@ import React, { Component } from 'react';
 import PropTypes  from 'prop-types';
 
 
-export  class  ColoredHeadingOne extends Component  {
- 
-
-  static  PropTypes = {
-
-    text:PropTypes.string,
-    color:PropTypes.string
-  };
-render(){
-  const {text , color } = this.props
-
-return <h1 style = {{color }}>
-  {text}
-  
-</h1>;
-
-
-}
-
-}
-
-export  class  ColoredHeadingTwo extends Component  {
+export default class  ColoredHeadingOne extends Component  {
  
 
   static  PropTypes = {
